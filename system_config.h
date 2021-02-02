29bcd76f1c9005482b7352bbe85d7a0fc4751e0a
Slawomir Kwasniak <slawomir.kwasniak@hensoldt-cyber.de>
2021-02-02 13:59:20 +0100

    [FIX] Remove unused Logs_Config.h
    File is not used anymore so can be removed.
    
    Fixes:
    SEOS-2087

--- system_config.h
+++ system_config.h
@@ -43,12 +43,3 @@
 //-----------------------------------------------------------------------------
 
 #define CHANMUX_ID_CRYPTOSERVER     101
-
-
-//-----------------------------------------------------------------------------
-// Logs
-//-----------------------------------------------------------------------------
-
-#define Logs_Config_LOG_STRING_SIZE         128
-#define Logs_Config_INCLUDE_LEVEL_IN_MSG    1
-#define Logs_Config_SYSLOG_LEVEL            Log_TRACE
